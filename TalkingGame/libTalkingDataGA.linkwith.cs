d18948dc3e61ea92eefab4c954305e44b198df5a
Jonathan Peppers <jonathan.peppers@gmail.com>
2014-06-11 10:49:38 -0500

    Turned on SmartLink for TalkingGame

--- TalkingGame/libTalkingDataGA.linkwith.cs
+++ TalkingGame/libTalkingDataGA.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libTalkingDataGA.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "Security", WeakFrameworks = "CoreTelephony AdSupport", LinkerFlags = "-lz")]
+[assembly: LinkWith ("libTalkingDataGA.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = true, Frameworks = "Security", WeakFrameworks = "CoreTelephony AdSupport", LinkerFlags = "-lz")]
