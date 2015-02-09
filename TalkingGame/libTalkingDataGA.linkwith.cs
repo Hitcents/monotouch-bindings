434a1336f711bd42f7d83adec028a348bb2a3271
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-02-09 15:31:39 -0600

    TalkingGame --> Unified API

--- TalkingGame/libTalkingDataGA.linkwith.cs
+++ TalkingGame/libTalkingDataGA.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
-using MonoTouch.ObjCRuntime;
+using ObjCRuntime;
 
 [assembly: LinkWith ("libTalkingDataGA.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = true, Frameworks = "Security", WeakFrameworks = "CoreTelephony AdSupport", LinkerFlags = "-lz")]
